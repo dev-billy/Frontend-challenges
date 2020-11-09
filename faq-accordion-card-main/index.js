b182be37e21e053134c29feaadeb6b823612f286
Billy <billypatrick767@gmail.com>
2020-11-09 21:25:35 +0300

    added listeners for all questions

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -1,13 +1,28 @@
-const question = document.getElementById('question1');
-const answer = document.getElementById('answer1');
+const question1 = document.getElementById('question1');
+const answer1 = document.getElementById('answer1');
+const question2 = document.getElementById('question2');
+const answer2 = document.getElementById('answer2');
+const question3 = document.getElementById('question3');
+const answer3 = document.getElementById('answer3');
+const question4 = document.getElementById('question4');
+const answer4 = document.getElementById('answer4');
+const question5 = document.getElementById('question5');
+const answer5 = document.getElementById('answer5');
 
+const answers = [answer1,answer2,answer3,answer4,answer5];
 
-question.addEventListener('click', ()=>{
-    showAnswer();
+[question1,question2,question3,question4,question5].forEach(item=>{
+    
+    item.addEventListener('click',()=>{
+        const itemId = item.getAttribute('id').substr(8,8) -1;        
+        showAnswer(answers[itemId],item);
+    })
 })
 
 
-function showAnswer(){
+
+function showAnswer(answer,question){
+    console.log(answer)
     if(answer.style.display==='block'){
         answer.style.display='none'
         question.childNodes[1].style.fontWeight = 400;
